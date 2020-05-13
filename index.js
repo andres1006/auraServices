73f27f20525dc65f91ae93ddb915adeb3397689d
andres1006 <andresagudelo1006@gmail.com>
2020-05-13 14:14:11 -0500

    make it better

--- index.js
+++ index.js
@@ -8,18 +8,13 @@ const app = express();
 const { PORT } = process.env;
 
 app.use(cors())
-// Convierte una petición recibida (POST-GET...) a objeto JSON
+
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 
 logRoute(app);
 
-// app.get('/', function(req, res){
-// 	res.status(200).send({
-// 		message: 'Hola Logs!'
-// 	});
-// });
 
 app.listen(PORT, function(){
 	console.log(`Server running in http://localhost:${PORT}`);
