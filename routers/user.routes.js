a1f93587811117c75f176a26b64630fd42402d25
GaboDotCodes <gabedu2000@gmail.com>
2020-07-08 11:29:26 -0500

    Just style

--- routers/user.routes.js
+++ routers/user.routes.js
@@ -1,20 +1,18 @@
-require('dotenv').config()
-const {Router} = require('express');
+require('dotenv').config();
+const { Router } = require('express');
 const router = Router();
 const userController = require('../controllers/user.controller');
-const Auth = require('../middlewares/auth'); 
+const Auth = require('../middlewares/auth');
 
 function userRoute(app) {
-    app.use('/api/user', router);
+  app.use('/api/user', router);
 
-    router.get('/',Auth.isAuth, userController.getUsers);
-    router.get('/:id',Auth.isAuth, userController.getUser);
-    router.post('/', userController.postUser);
-    router.delete('/:id',Auth.isAuth,userController.deleteUser);
-    router.put('/:id',Auth.isAuth,userController.putUser);
-    router.post('/autenticar', userController.signIn);    
+  router.get('/', Auth.isAuth, userController.getUsers);
+  router.get('/:id', Auth.isAuth, userController.getUser);
+  router.post('/', userController.postUser);
+  router.delete('/:id', Auth.isAuth, userController.deleteUser);
+  router.put('/:id', Auth.isAuth, userController.putUser);
+  router.post('/autenticar', userController.signIn);
+}
 
-} 
-
-module.exports= userRoute;
-
+module.exports = userRoute;
