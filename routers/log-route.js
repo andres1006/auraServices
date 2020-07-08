941ab4aeb282d1702b1aaa4e1e900c93aeea2c8e
GaboDotCodes <gabedu2000@gmail.com>
2020-07-08 11:27:48 -0500

    Just style

--- routers/log-route.js
+++ routers/log-route.js
@@ -1,46 +1,43 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
-const {pushLog, searchLog} = require('../service/log-service');
+const { pushLog, searchLog } = require('../service/log-service');
 
 function logRoute(app) {
-    const router = express.Router();
-    app.use('/api/log', router);
-  
-    router.post('/', async function(req, res, next) {
-      try {
-        const { body } = req;
-        const resLogPush = await pushLog(body);
+  const router = express.Router();
+  app.use('/api/log', router);
+
+  router.post('/', async function (req, res, next) {
+    try {
+      const { body } = req;
+      const resLogPush = await pushLog(body);
+      res.status(200).json({
+        data: resLogPush,
+        message: 'Log agregado correctamente',
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  router.get('/listarlogs', async function (req, res, next) {
+    try {
+      const { body } = req;
+      const resLogPush = await searchLog(body);
+
+      if (resLogPush) {
         res.status(200).json({
           data: resLogPush,
-          message: 'Log agregado correctamente'
+          message: 'Log encontrado',
         });
-      } catch (err) {
-        next(err);
+      } else {
+        res.status(204).json({
+          message: 'No hay logs',
+        });
       }
-    });
+    } catch (err) {
+      next(err);
+    }
+  });
+}
 
-    router.get('/listarlogs', async function(req, res, next) {
-      try {
-        const { body } = req;
-        const resLogPush = await searchLog(body);
-
-        if (resLogPush){
-          res.status(200).json({
-            data: resLogPush,
-            message: 'Log encontrado'
-          });
-        }else{
-          res.status(204).json({
-            message: 'No hay logs'
-          });
-        }
-
-
-      } catch (err) {
-        next(err);
-      }
-    });
-  
-  }
-  
-  module.exports = logRoute;
+module.exports = logRoute;
